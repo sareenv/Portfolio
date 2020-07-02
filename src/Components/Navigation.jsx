33e0c95f75f2e315a28bc0a65fea1c63ef326c79
Vinayak <sareenv@uni.coventry.ac.uk>
2020-07-02 21:22:26 +0100

    styles folder added

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -11,7 +11,7 @@ class Navigation extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            selected: 0
+            selected: -1
         }
     }
   
@@ -27,18 +27,18 @@ class Navigation extends React.Component {
             <Router> 
                 <Navbar style={{backgroundColor: '#1d3557'}} expand="lg">
                     <Nav className="m-auto">
-                        <Nav.Link as={NavLink} to="/" onClick={() => this.changeIndex(0)} exact style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 0) ? '#e43f5a' : '#1b1b2f'}}>Projects</Nav.Link>
-                        <Nav.Link as={NavLink} to="/blog" onClick={() => this.changeIndex(1)} style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 1) ? '#e43f5a' : '#1b1b2f'}}>Blogs</Nav.Link>
-                        <Nav.Link as={NavLink} to="/hackathons" onClick={() => this.changeIndex(2)} exact style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 2) ? '#e43f5a' : '#1b1b2f'}}> Hackathons </Nav.Link>
-                        <Nav.Link as={NavLink} to="/research" onClick={() => this.changeIndex(3)} style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 3) ? '#e43f5a' : '#1b1b2f'}}>Research</Nav.Link>
-                        <Nav.Link as={NavLink} to="/experience" onClick={() => this.changeIndex(4)} style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 4) ? '#e43f5a' : '#1b1b2f'}}> Contact Me </Nav.Link>
+                        <Nav.Link as={NavLink} to="/" activeClassName="is-active" exact style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 0) ? '#e43f5a' : '#1b1b2f'}}>Projects</Nav.Link>
+                        <Nav.Link as={NavLink} to="/blog" activeClassName="is-active" style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 1) ? '#e43f5a' : '#1b1b2f'}}>Blogs</Nav.Link>
+                        <Nav.Link as={NavLink} to="/hackathons" activeClassName="is-active" exact style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 2) ? '#e43f5a' : '#1b1b2f'}}> Hackathons </Nav.Link>
+                        <Nav.Link as={NavLink} to="/research" activeClassName="is-active" style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 3) ? '#e43f5a' : '#1b1b2f'}}>Research</Nav.Link>
+                        <Nav.Link as={NavLink} to="/experience" activeClassName="is-active" style={{padding: 10, color: 'white', backgroundColor: (this.state.selected === 4) ? '#e43f5a' : '#1b1b2f'}}> Contact Me </Nav.Link>
                     </Nav>
                 </Navbar>
-                <Route path="/" exact component={Home} />
-                <Route path="/blog" exact component={BlogPage} />
-                <Route path="/hackathons" exact component={HackathonPage} />
-                <Route path="/research" exact component={ResearchPage} />
-                <Route path="/experience" exact component={ResearchPage} />
+                    <Route path="/" exact component={Home} />
+                    <Route path="/blog" exact component={BlogPage} />
+                    <Route path="/hackathons" exact component={HackathonPage} />
+                    <Route path="/research" exact component={ResearchPage} />
+                    <Route path="/experience" exact component={ResearchPage} />
             </Router>
             </div>
         )
