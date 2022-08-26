c133811a8917fef9ba5dec4269b712bf0331e649
Vinayak <sareenv026@outlook.com>
2022-08-26 02:44:56 -0400

    content changes

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,17 +1,36 @@
 
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import { Container, Button } from 'react-bootstrap';
+import '../../Styles/contact.scss'
 
 const Contact = () => {
     return (
-        <div style={{ padding: 10, background: 'red'}} > 
+        <Container fluid='sm' className='contact-form'> 
+            <center>
+
             <h2> Get in Touch </h2>
-            <form> 
-                <input placeholder="Enter your name"/><br />
-                <input placeholder="Enter your email"/> <br />
-                <textarea placeholder="Your message" /><br /><br />
-                <button> Send Query </button>
-            </form>
-        </div>
+
+            </center>
+            <Form>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label htmlFor="email">Full Name</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label htmlFor="email">Email</Form.Label>
+                    <Form.Control type="email" placeholder="name@example.com" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label htmlFor="email">Your Message</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                <Button variant="primary" type="submit"> Send Message </Button>
+            </Form>
+        </Container>
     )
 }
 
