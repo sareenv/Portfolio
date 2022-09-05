c0b2317101f5f2504bfc3a9fcbc35f34475a6e31
Vinayak <sareenv026@outlook.com>
2022-09-05 13:52:02 -0400

    project section edit

--- src/Components/Projects/ProjectsPage.jsx
+++ src/Components/Projects/ProjectsPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Project from "./Projects";
-import {Row, Col, Image, Button} from 'react-bootstrap'
+import {Row, Col, Image, Button, Container} from 'react-bootstrap'
 const ProjectsPage = () => {
     return (
         <div>
-            <div style={{backgroundColor: 'white', minHeight: '65%', width: 'auto', margin: '1rem'}}>
+            <Container style={{marginTop: '1rem'}}>
                 <Row>
                     <Col lg={6} md={12} sm={12}>
                     <Image fluid={true} src="https://letsbuildthatapp-videos.s3.us-west-2.amazonaws.com/194badd9-b719-495b-bc77-3737115a9524"/> 
@@ -20,7 +20,7 @@ const ProjectsPage = () => {
                         </div>
                     </Col>
                 </Row>
-            </div>
+            </Container>
             <Project />
         </div>
     )
