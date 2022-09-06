4e9cc5c4a6e2f940305adbe63166415f70d7b223
Vinayak <sareenv026@outlook.com>
2022-09-06 17:55:40 -0400

    changes

--- src/Components/Personal/Services.jsx
+++ src/Components/Personal/Services.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Row, Col, Container, Badge} from 'react-bootstrap'
 
 const Service = () => {
     return (
-        <div className="services">
-            This is the services section
-        </div>
+        <Container style={{marginTop: '1rem'}}>
+            <Row>
+                <Col sm={12} md={12} lg={6} style={{backgroundColor: 'red', minHeight: '20rem'}}>
+                
+                </Col>
+
+                <Col sm={12} md={12} lg={6} style={{backgroundColor: 'blue'}}>
+            
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
