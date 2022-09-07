55c23c26b01634889ca06cf6a2ae5fbc95053ede
Vinayak <sareenv026@outlook.com>
2022-09-06 20:26:24 -0400

    changes

--- src/Components/Personal/Services.jsx
+++ src/Components/Personal/Services.jsx
@@ -1,16 +1,69 @@
 import React from 'react';
-import {Row, Col, Container, Badge} from 'react-bootstrap'
+import {Row, Col, Container, Table} from 'react-bootstrap'
 
 const Service = () => {
     return (
         <Container style={{marginTop: '1rem'}}>
             <Row>
-                <Col sm={12} md={12} lg={6} style={{backgroundColor: 'red', minHeight: '20rem'}}>
-                
+                <Col sm={12} md={12} lg={6} style={{backgroundColor: 'white', minHeight: '20rem'}}>
+                    <h3> Skills </h3>
+                    <hr/>
+
+                    <Table fluid={true}>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Type</th>
+                                <th>Technology</th>
+                                <th>Experience</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Backend</td>
+                            <td>Node.js </td>
+                            <td>
+                                <li style={{fontSize: '0.8rem'}}>Express</li>
+                                <li style={{fontSize: '0.8rem'}}>Koa</li>
+                                <li style={{fontSize: '0.8rem'}}>Jest</li>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>2</td>
+                            <td>Frontend</td>
+                            <td> React </td>
+                            <td>
+                                <li style={{fontSize: '0.8rem'}}> Redux </li>
+                                <li style={{fontSize: '0.8rem'}}> Context </li>
+                                <li style={{fontSize: '0.8rem'}}> Functional </li>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>3</td>
+                            <td>Mobile </td>
+                            <td> iOS </td>
+                            <td>
+                                <li style={{fontSize: '0.8rem'}}> UIKit </li>
+                                <li style={{fontSize: '0.8rem'}}> AVKit </li>
+                                <li style={{fontSize: '0.8rem'}}> Swift </li>
+                                <li style={{fontSize: '0.8rem'}}> MapKit </li>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+
+
+
+                    </Table>
+
                 </Col>
 
                 <Col sm={12} md={12} lg={6} style={{backgroundColor: 'blue'}}>
-            
+                    
                 </Col>
             </Row>
         </Container>
