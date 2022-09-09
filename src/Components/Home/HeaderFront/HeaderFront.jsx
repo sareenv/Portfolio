55df94757d02f3f812326150805ca2e36f751d85
Vinayak <sareenv026@outlook.com>
2022-09-09 13:33:37 -0400

    project changes

--- src/Components/Home/HeaderFront/HeaderFront.jsx
+++ src/Components/Home/HeaderFront/HeaderFront.jsx
@@ -13,14 +13,14 @@ const HeaderFront = () => {
                 <Image thumbnail className="circularimage" src = {image} alt='noImg' />
                 <div style={{display: 'flex', flexDirection: 'column', padding: 0, margin: 0}}>
                     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-                        <p style = {{color: '#e63946', fontSize: '1.6rem', marginTop: 15, padding: 3}}>  Hi <span role="img" aria-label="hi">👋 </span> I'm </p>
-                        <p style = {{color: '#f1faee', fontSize: '1.6rem', marginTop: 15}}> Vinayak Sareen  </p>
+                        <h3 style = {{color: '#e63946', marginTop: 15, padding: 3}}>  Hi <span role="img" aria-label="hi"> 👋 </span> I'm </h3>
+                        <h3 style = {{color: '#f1faee', marginTop: 15}}> Vinayak Sareen  </h3>
                     </div>
                     <p style={{color: '#f1faee', marginTop: '0.1em', fontSize: '1em', textAlign: 'center'}}> 
-                         I'm enthusiastic Masters (MEng) Student at Concordia University with Software Development experience </p>
+                         I'm enthusiastic Masters (MEng) Student at Concordia University. </p>
                 </div>
                 <AnimatedTitles /> 
-                <Button variant="light"> <span style={{fontSize: '1em'}}> 🧑‍💻  </span> Resume </Button> 
+                <Button variant="light"> <span style={{fontSize: '1em'}} role="img" aria-label="cv"> 🧑‍💻  </span> Resume </Button> 
             </div>
             <div style={{color: 'white', display: 'flex', marginTop: -40, justifyContent: 'flex-end', marginRight: 10}}> 
                 <a style={{textDecoration: 'none', color: 'white'}} href="https://github.com/sareenv"> <FaGithub style = {{marginRight: 15, fontSize: 20}}/> </a>
