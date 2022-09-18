d4418e1b59e9f81bc0ff933504474f1e032272a4
Vinayak <sareenv026@outlook.com>
2022-09-17 22:10:05 -0400

    changes

--- src/Components/Projects/ProjectDetails.jsx
+++ src/Components/Projects/ProjectDetails.jsx
@@ -3,53 +3,78 @@ import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import React  from 'react';
 import { useEffect, useState } from 'react';
 import { FaGithub, FaVideo } from 'react-icons/fa';
-
 import '../../Styles/product-details.scss'
 
 const ProjectDetails = () => {
+    const [displayImage, setDisplayImage] = useState("");
+    useEffect(() => {
+        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato5.png")
+    }, [])
+
     return (
-        <Container>
-           
+        <Container style={{paddingBottom: '2rem', backgroundColor: 'white'}}>
             <Row style={{marginTop: '1rem'}}>
                 <Col lg={6} style={{backgroundColor: 'white'}}>
                     <Image
                         fluid
                         thumbnail = {true}
-                        src='https://sareenv-projects.s3.amazonaws.com/images/cinemato.png'>
+                        src={displayImage}>
                     </Image>
                         <Row style={{display: 'flex', justifyContent: 'start'}}>
-                            
-                            
-
                             <Col lg={3} sm={3} xs={3} style={{marginTop: '0.7rem'}}>
                                 <Image fluid thumbnail={true}
-                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato2.png'></Image>
+                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato2.png'
+                                    onClick={() =>{
+                                        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato2.png")
+                                    }}
+                                    ></Image>
                             </Col>
 
                             <Col lg={3} sm={3} xs={3} style={{marginTop: '0.7rem'}}>
                                 <Image fluid thumbnail={true}
-                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato3.png'></Image>
+                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato3.png' 
+                                    onClick={ () => {
+                                        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato3.png")
+                                    }}></Image>
                             </Col>
 
                             <Col lg={3} sm={3} xs={3} style={{marginTop: '0.7rem'}}>
                                 <Image fluid thumbnail={true}
-                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato4.png'></Image>
+                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato4.png'
+                                    onClick={() => {
+                                        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato4.png")
+                                    }}
+                                    ></Image>
                             </Col>
 
                             <Col lg={3} sm={3} xs={3} style={{marginTop: '0.7rem'}}>
                                 <Image fluid thumbnail={true}
-                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato5.png'></Image>
+                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato5.png' 
+                                    onClick={() => {
+                                        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato5.png")
+                                    }}
+                                    ></Image>
                             </Col>
 
                             <Col lg={3} sm={3} xs={3} style={{marginTop: '0.7rem'}}>
                                 <Image fluid thumbnail={true}
-                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato6.png'></Image>
+                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato6.png' 
+                                    onClick={() => {
+                                        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato6.png")
+                                    }}
+                                    ></Image>
                             </Col>
 
                             <Col lg={3} sm={3} xs={3} style={{marginTop: '0.7rem'}}>
                                 <Image fluid thumbnail={true}
-                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato7.png'></Image>
+                                    src='https://sareenv-projects.s3.amazonaws.com/images/cinemato7.png' 
+                                    onClick={() => {
+                                        setDisplayImage("https://sareenv-projects.s3.amazonaws.com/images/cinemato7.png")
+                                    }}
+                                    ></Image>
                             </Col>
+
+                  
                         </Row>
 
                         
