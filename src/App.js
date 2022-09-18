0800119b75cfa058744ac6d3982591306fcc3d5d
Vinayak <sareenv026@outlook.com>
2022-09-17 23:29:40 -0400

    removed dependency

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Navigation from './Components/Navigation'
 import './Styles/base.scss'
-import SafeArea from 'react-safe-area-component'
 
 function App() {
   return (
-    <SafeArea top>
       <Navigation /> 
-    </SafeArea>
   );
 }
 
