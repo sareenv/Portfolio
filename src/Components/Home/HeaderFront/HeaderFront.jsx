0f1a14e180e238197364004fda1cf23aff728bd5
Vinayak <sareenv026@outlook.com>
2022-11-18 20:56:30 -0500

    profile animation added

--- src/Components/Home/HeaderFront/HeaderFront.jsx
+++ src/Components/Home/HeaderFront/HeaderFront.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 import AnimatedTitles from './AnimatedtTitles'
 import image from '../../../Assets/me.jpeg'
 import { Button, Image } from 'react-bootstrap'
 import { FaGithub, FaLinkedin, FaDiscord } from "react-icons/fa"
+import {motion} from 'framer-motion'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../../../Styles/frontheader.scss'
 
 const HeaderFront = () => {
+    const [rotate, setRotate] = useState(false);
     return (
         <div>
             <div className="frontheader">
-                <Image thumbnail className="circularimage" src = {image} alt='noImg' />
+                <motion.div animate={{ rotate: rotate ? 360 : 0}} onClick={() => {
+                    setRotate(!rotate)
+                }} onHoverStart={() => {
+                    setRotate(!rotate)
+                }}>
+                    <Image thumbnail className="circularimage" src = {image} alt='noImg' />
+                </motion.div>
+
                 <div style={{display: 'flex', flexDirection: 'column', padding: 0, margin: 0}}>
                     <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
                         <h3 style = {{color: '#e63946', marginTop: 15, padding: 3}}>  Hi <span role="img" aria-label="hi"> 👋 </span> I'm </h3>
