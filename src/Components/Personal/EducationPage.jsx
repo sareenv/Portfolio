cd73213be68c93fc7188a209eabe9d0f629fa219
Vinayak <sareenv026@outlook.com>
2023-04-14 15:13:29 -0400

    fixed alignment issue and added the ceridian experience

--- src/Components/Personal/EducationPage.jsx
+++ src/Components/Personal/EducationPage.jsx
@@ -2,6 +2,7 @@
 import React, {useEffect} from 'react';
 import {Container, Row, Col} from 'react-bootstrap'
 import {Education} from '../Personal/About'
+import Footer from '../Footer';
 import { ConcordiaDetails, AWSCertification, CoventryDetails, DockerCertification } from './details';
 import ReactGA from 'react-ga'
 const EducationPage = () => {
@@ -11,13 +12,13 @@ const EducationPage = () => {
     }, [])
 
     return (
-        <div style={{backgroundColor: 'white', paddingBottom: '2rem'}}>
+        <div style={{backgroundColor: 'white', paddingBottom: '5rem'}}>
             <Container style={{marginTop: '2rem'}}>
                 <h3 style={{marginTop: '2rem'}}>Higher Education </h3>
                 <hr/>
-                <Row style={{display: 'flex', flexWrap: 'wrap'}}>
+                <Row >
                     
-                    <Col sm={12} md={11} lg={6}  >
+                    <Col sm={12} md={11} lg={6}  style={{marginTop: '1rem'}}>
                         <Education 
                             image={ConcordiaDetails.image}
                             qualification={ConcordiaDetails.qualification} 
@@ -29,7 +30,7 @@ const EducationPage = () => {
                         />
                     </Col>
                 
-                    <Col sm={12} md={11} lg={6} >
+                    <Col sm={12} md={11} lg={6} style={{marginTop: '1rem'}} >
                         <Education 
                             
                             image={CoventryDetails.image}
@@ -47,35 +48,35 @@ const EducationPage = () => {
                     </Col>
                 </Row>
 
-                    <h3 style={{marginTop: '2rem'}}>Certifications & Licensing Education </h3>
-                    <Row>
+                <h3 style={{marginTop: '2rem'}}>Certifications & Licensing Education </h3>
+                <Row>
 
-                    <Col sm={12} md={12} lg={6} style={{marginBottom: '1rem'}}>
-                        <Education 
-                            image={DockerCertification.image}
-                            qualification={DockerCertification.qualification} 
-                            instition={DockerCertification.instition} 
-                            visible={true}
-                            bnttitle={'View Credentials'}
-                            credLink={DockerCertification.credLink}
-                            issued={DockerCertification.issued}
-                            certificateID={DockerCertification.certificateID}
-                        />
-                    </Col>
-
-                    <Col sm={12} md={12} lg={6}>
-                        <Education 
-                            image={AWSCertification.image}
-                            qualification={AWSCertification.qualification} 
-                            instition={AWSCertification.instition} 
-                            visible={true}
-                            bnttitle={'View Credentials'}
-                            credLink={AWSCertification.credLink}
-                            issued={AWSCertification.issued}
-                        />
-                    </Col>
-                </Row>
+                        <Col sm={12} md={12} lg={6} style={{marginTop: '1rem'}}>
+                            <Education 
+                                image={DockerCertification.image}
+                                qualification={DockerCertification.qualification} 
+                                instition={DockerCertification.instition} 
+                                visible={true}
+                                bnttitle={'View Credentials'}
+                                credLink={DockerCertification.credLink}
+                                issued={DockerCertification.issued}
+                                certificateID={DockerCertification.certificateID}
+                            />
+                        </Col>
+                        <Col sm={12} md={12} lg={6} style={{marginTop: '1rem'}}>
+                            <Education 
+                                image={AWSCertification.image}
+                                qualification={AWSCertification.qualification} 
+                                instition={AWSCertification.instition} 
+                                visible={true}
+                                bnttitle={'View Credentials'}
+                                credLink={AWSCertification.credLink}
+                                issued={AWSCertification.issued}
+                            />
+                        </Col>
+                    </Row>
             </Container>
+            <Footer />
         </div>
     )
 }
