2351dadab472152bc70ef6dd4171b088db6923fd
Vinayak <contact@sareenv.com>
2023-12-18 17:21:33 -0500

    fixed height issues and added the shimmer effect on the projects page

--- src/Services/Projects.js
+++ src/Services/Projects.js
@@ -1,15 +1,29 @@
 
 const baseUrl = "https://api.sareenv.com/api/v1"
 
+var projectCache = []
+const projectMap = new Map();
+
 const downloadProject = () => {
+    if (projectCache.length >= 1) { 
+        console.log("returning from cache")
+        return projectCache 
+    }
     const url = `${baseUrl}/projects`
     const data = fetch(url).then(data => data.json())
+    projectCache = data
     return data
 }
 
 const downloadProjectByID = (id) => {
+    // return from the memory cache.
+    if(projectMap.has(id)) { 
+        console.log("returning from cache")
+        return projectMap.get(id) 
+    }
     const url = `${baseUrl}/project/${id}`
     const data = fetch(url).then(data => data.json())
+    projectMap.set(id, data)
     return data
 }
 
