8b6b029be6668c5462540e724fcdc91bbedd83e0
Vinayak <sareenv026@outlook.com>
2025-08-31 16:19:20 -0400

    Update Projects.js

--- src/Services/Projects.js
+++ src/Services/Projects.js
@@ -1,15 +1,15 @@
 
-const baseUrl = "http://api.sareenv.com/api/v1"
+const baseUrl = "https://api.sareenv.com/api/v1"
 
 const downloadProject = () => {
     const url = `${baseUrl}/projects`
-    const data = fetch(url, {referrerPolicy: "unsafe-url"}).then(data => data.json())
+    const data = fetch(url).then(data => data.json())
     return data
 }
 
 const downloadProjectByID = (id) => {
     const url = `${baseUrl}/project/${id}`
-    const data = fetch(url, {referrerPolicy: "unsafe-url"}).then(data => data.json())
+    const data = fetch(url).then(data => data.json())
     return data
 }
 
