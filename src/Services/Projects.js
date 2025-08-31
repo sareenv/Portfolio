7395b1e68d50966b8103d5e237518bf678697e17
Vinayak <sareenv026@outlook.com>
2025-08-31 15:54:45 -0400

    Update Projects.js

--- src/Services/Projects.js
+++ src/Services/Projects.js
@@ -5,24 +5,15 @@ var projectCache = []
 const projectMap = new Map();
 
 const downloadProject = () => {
-    if (projectCache.length >= 1) { 
-        console.log("returning from cache")
-        return projectCache 
-    }
     const url = `${baseUrl}/projects`
-    const data = fetch(url).then(data => data.json())
+    const data = fetch(url, {referrerPolicy: "unsafe-url"}).then(data => data.json())
     projectCache = data
     return data
 }
 
 const downloadProjectByID = (id) => {
-    // return from the memory cache.
-    if(projectMap.has(id)) { 
-        console.log("returning from cache")
-        return projectMap.get(id) 
-    }
     const url = `${baseUrl}/project/${id}`
-    const data = fetch(url).then(data => data.json())
+    const data = fetch(url, {referrerPolicy: "unsafe-url"}).then(data => data.json())
     projectMap.set(id, data)
     return data
 }
